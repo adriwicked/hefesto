0840b19c7432e199c399976419d3e904b99ea790
Adrián Moreno <adrianmn90@gmail.com>
2023-05-07 21:08:49 +0200

    feat() implement get_workout_sessions method

--- src/user/domain/user.py
+++ src/user/domain/user.py
@@ -1,15 +1,14 @@
 class User:
-    def __init__(
-        self,
-        name: str,
-        email: str
-    ):
-        self.name = name
-        self.email = email
-        self.workout_sessions = []
+    def __init__(self, name: str, email: str):
+        self._name = name
+        self._email = email
+        self._workout_sessions = []
+
+    def get_workout_sessions(self):
+        return self._workout_sessions
 
     def add_workout_session(self, workout_session):
-        if workout_session in self.workout_sessions:
+        if workout_session in self._workout_sessions:
             raise ValueError('The workout session already exists')
-        
-        self.workout_sessions.append(workout_session)
+
+        self._workout_sessions.append(workout_session)
