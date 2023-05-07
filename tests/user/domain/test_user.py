26d0c3e1984e49b387713272f9d4c56552cd6f19
adrian moreno <adrian.moreno.navarro.contractor@bbva.com>
2023-05-07 21:08:49 +0200

    feat() implement get_workout_sessions method

--- tests/user/domain/test_user.py
+++ tests/user/domain/test_user.py
@@ -7,15 +7,20 @@ from src.user.domain.user import User
 class TestUser(unittest.TestCase):
     def test_init(self):
         user = User(name='Adrián', email='adrian@example.com')
-        self.assertEqual(user.name, 'Adrián')
-        self.assertEqual(user.email, 'adrian@example.com')
+        self.assertEqual(user._name, 'Adrián')
+        self.assertEqual(user._email, 'adrian@example.com')
+
+    def test_get_workout_sessions(self):
+        user = User(name='Adrián', email='adrian@example.com')
+        self.assertEqual(user.get_workout_sessions(), [])
 
     def test_add_workout_session(self):
         user = User(name='Adrián', email='adrian@example.com')
         workout_session = date.today()
         user.add_workout_session(workout_session)
-        self.assertEqual(len(user.workout_sessions), 1)
-        self.assertEqual(user.workout_sessions[0], workout_session)
+        workout_sessions = user.get_workout_sessions()
+        self.assertEqual(len(workout_sessions), 1)
+        self.assertEqual(workout_sessions[0], workout_session)
 
     def test_add_duplicated_workout_session(self):
         user = User(name='Adrián', email='adrian@example.com')
