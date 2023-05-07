ac6718e602e286b44f6122d32e66b34ae80aab05
Adrián Moreno <adrianmn90@gmail.com>
2023-05-07 22:53:21 +0200

    feat() implement user comparison

--- tests/user/domain/test_user_id.py
+++ tests/user/domain/test_user_id.py
@@ -10,3 +10,13 @@ class TestUserID(unittest.TestCase):
         self.assertIsInstance(user_id.value, str)
         uuid4_regex = r'^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$'
         self.assertRegex(user_id.value, uuid4_regex)
+
+    def test_compare_same_user_ids(self):
+        user_id1 = UserID(value='abc')
+        user_id2 = UserID(value='abc')
+        self.assertEqual(user_id1, user_id2)
+
+    def test_compare_different_user_ids(self):
+        user_id1 = UserID()
+        user_id2 = UserID()
+        self.assertNotEqual(user_id1, user_id2)
