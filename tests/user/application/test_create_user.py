20844f55fe2e951fce074ba86db80bd92cd0e00c
adrian moreno <adrianmn90@gmail.com>
2023-06-09 21:52:09 +0200

    feat() create user raises error if name or email are not provided

--- tests/user/application/test_create_user.py
+++ tests/user/application/test_create_user.py
@@ -13,3 +13,10 @@ class TestCreateUser(unittest.TestCase):
         created_user = create_user(name, email)
 
         self.assertEqual(created_user._name, name)
+
+    def test_create_user_without_name_email_raise_error(self):
+        user_repository = UserRepository()
+        create_user = get_create_user(user_repository)
+
+        with self.assertRaises(AssertionError):
+            create_user(name='', email='')
