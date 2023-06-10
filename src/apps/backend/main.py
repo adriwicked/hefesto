5bc37416d3f88168057fc22e922934face812402
adrian moreno <adrianmn90@gmail.com>
2023-06-10 19:29:32 +0200

    feat() implement login

--- src/apps/backend/main.py
+++ src/apps/backend/main.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, request, make_response
+from flask_cors import CORS
+from google.auth.transport import requests
+from google.oauth2.id_token import verify_firebase_token
 
 app = Flask(__name__)
+CORS(app)
 
+firebase_request = requests.Request()
 
 @app.route('/')
 def root():
     return {'list': [1, 2, 3]}
+
+@app.route('/login', methods=['POST'])
+def login():
+    token = request.headers.get('Authentication').replace('Bearer ', '')
+
+    try:
+        claims = verify_firebase_token(token, firebase_request)
+        print('claims', claims)
+    except ValueError as exc:
+        error_message = str(exc)
+        print('error_message', error_message)
+
+    return make_response('ok', 200)
 
 
 if __name__ == '__main__':
